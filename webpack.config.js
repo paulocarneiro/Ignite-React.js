ad2fb173f320ef5e23ad015731fad6226f1a206b
paulocarneiro <paulocarneiro1993@gmail.com>
2021-03-10 14:31:03 -0300

    webpack

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
         rules: [
             {
                 test:/\.jsx$/,
-                exclude: /node_modules/,
+                exclude: /node_modules/,,
                 use: 'babel-loader',
             }
         ],
